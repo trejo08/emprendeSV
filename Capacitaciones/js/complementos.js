c6b066c27432c56d88c09577ceb0a0f62bd8c7ba
XaiMendez <shavy_00@hotmail.com>
2015-02-28 23:40:01 -0600

    vistas_capacitación v2

--- Capacitaciones/js/complementos.js
+++ Capacitaciones/js/complementos.js
@@ -2,4 +2,39 @@ $(function(){
 	new WOW().init();
 })
 
+$(document).ready(function() {
+$(":image#boton").click(function(){
+document.getElementById('#caja').innerHTML = "ADIOS";
+});
+});
+
+$(document).ready(function() {
+$(":image#boton1").click(function(){
+	$("#caja").append("<span style='font-size: 20px;'></span>");
+$("#caja").append("<span style='font-size: 20px;'>Descripción del Tema 2</span>");
+});
+});
+
+$(document).ready(function() {
+$(":image#boton2").click(function(){
+	$("#caja").append("<span style='font-size: 20px;'></span>");
+$("#caja").append("<span style='font-size: 20px;'>Descripción del Tema 3</span>");
+});
+});
+
+function cambiar()
+{
+  document.getElementById('caja').innerHTML = "::Descripción del Tema 1::";
+}
+
+function cambiar1()
+{
+  document.getElementById('caja').innerHTML = "::Descripción del Tema 2::";
+}
+
+function cambiar2()
+{
+  document.getElementById('caja').innerHTML = "::Descripción del Tema 3::";
+}
+
 
